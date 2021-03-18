4258995c42f14d7c83ed120d3d44d2ff94329152
Ashraful-Mijan <ashrafulmijan5@gmail.com>
2021-03-19 00:39:06 +0600

    first-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle'
+
+function App() {
+  return (
+    <div >
+      <h3>welcome react</h3>
+    </div>
+  );
+}
+
+export default App;
